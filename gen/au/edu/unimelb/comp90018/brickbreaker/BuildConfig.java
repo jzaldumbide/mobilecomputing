45ed5f8d1f84046e93e72737cc4d3068e88b59a9
Oscar Correa <oscarcorreag@gmail.com>
2014-08-12 20:30:21 +1000

    Initial commit.

---
+++ gen/au/edu/unimelb/comp90018/brickbreaker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package au.edu.unimelb.comp90018.brickbreaker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
