b937c6326bc327a20185a88f09b8eb9f1e6f2cc1
Oscar Correa <oscarcorreag@gmail.com>
2014-08-12 21:05:20 +1000

    Only formatting :(

--- src/au/edu/unimelb/comp90018/brickbreaker/framework/FileIO.java
+++ src/au/edu/unimelb/comp90018/brickbreaker/framework/FileIO.java
@@ -7,8 +7,8 @@ import java.io.OutputStream;
 public interface FileIO {
 
 	public InputStream readAsset(String fileName) throws IOException;
-	
+
 	public InputStream readFile(String fileName) throws IOException;
-	
-    public OutputStream writeFile(String fileName) throws IOException;
+
+	public OutputStream writeFile(String fileName) throws IOException;
 }
