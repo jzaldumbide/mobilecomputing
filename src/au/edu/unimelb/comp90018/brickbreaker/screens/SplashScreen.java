0da9d955578ea829f06c4ebe9a01de6a714fd103
jzaldumbide <jzaldumbide@gmail.com>
2014-10-05 10:44:42 +1100

    SplashScreen verification, comprueba que el archivo con los datos del usuario exista, si no existe(primera vez), lo crea. Si ya existe, va directamente al menu

--- src/au/edu/unimelb/comp90018/brickbreaker/screens/SplashScreen.java
+++ src/au/edu/unimelb/comp90018/brickbreaker/screens/SplashScreen.java
@@ -4,46 +4,60 @@ import au.edu.unimelb.comp90018.brickbreaker.BrickBreaker;
 import au.edu.unimelb.comp90018.brickbreaker.framework.util.Assets;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.TimeUtils;
 
-public class SplashScreen extends ScreenAdapter {
+public class SplashScreen extends ScreenAdapter implements TextInputListener {
 	BrickBreaker game;
+	private long startTime;
 
 	OrthographicCamera guiCam;
+	public final static String file = "brickbreaker.data";
 
 	public SplashScreen(BrickBreaker game) {
+		// User user = null;
 
 		this.game = game;
 		guiCam = new OrthographicCamera(900, 1024);
 		guiCam.position.set(900 / 2, 1024 / 2, 0);
+
+		startTime = TimeUtils.millis();
+
+		// return;
 	}
 
 	public void update() {
 	}
 
 	public void draw() {
-		
 		GL20 gl = Gdx.gl;
-		
-		gl.glViewport(
-				(int) game.viewport.x, 
-				(int) game.viewport.y, 
-				(int) game.viewport.width,
-				(int) game.viewport.height
-				);
-		
-		gl.glClearColor(0, 0, 0, 1);
 		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
 		guiCam.update();
 
 		game.batcher.setProjectionMatrix(guiCam.combined);
 		game.batcher.disableBlending();
 		game.batcher.begin();
 		game.batcher.draw(Assets.splashScreen, 0, 0, 900, 1024);
-		game.batcher.end();		
+		game.batcher.end();
+
+		try {
+			FileHandle filehandle = Gdx.files.external(file);
+
+			if (!filehandle.exists()) {
+				if (TimeUtils.millis() > (startTime + 3000))
+
+					game.setScreen(new CreateUserScreen(game));
+
+			} else {
+				if (TimeUtils.millis() > (startTime + 3000))
+					game.setScreen(new MenuScreen(game));
+			}
+		} catch (Throwable e) {
+		}
 
 	}
 
@@ -58,4 +72,15 @@ public class SplashScreen extends ScreenAdapter {
 		super.dispose();
 	}
 
+	@Override
+	public void canceled() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void input(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
 }
