cbc29c71b9579e2509da353ad94f59538e901634
jzaldumbide <jzaldumbide@gmail.com>
2014-10-09 16:24:01 +1100

    HelpScreen

--- src/au/edu/unimelb/comp90018/brickbreaker/screens/HelpScreen.java
+++ src/au/edu/unimelb/comp90018/brickbreaker/screens/HelpScreen.java
@@ -19,13 +19,22 @@ public class HelpScreen extends ScreenAdapter {
 	OrthographicCamera guiCam;
 	Vector3 touchPoint;
 
-	String textString;
+	String textHeadline = "1/5";
+	String textHint = "Hint1";
+	String screenshot = "help1.png";
+	String hint1; 
+	String hint2;
+	String hint3; 
+	String hint4; 
+	String hint5;
 	private Button btnBack;
+	private Button btnNext;
+	int counter = 0;
 
 	public HelpScreen(BrickBreaker game) {
 
 		this.game = game;
-		
+
 		guiCam = new OrthographicCamera(Settings.TARGET_WIDTH,
 				Settings.TARGET_HEIGHT);
 		guiCam.position.set(Settings.TARGET_WIDTH / 2,
@@ -34,25 +43,34 @@ public class HelpScreen extends ScreenAdapter {
 		touchPoint = new Vector3();
 
 		btnBack = new Button(20, 20, ButtonSize.MEDIUM_SQUARE);
+		btnNext = new Button(20, 50, ButtonSize.MEDIUM_SQUARE);
 
-		textString = "Under Construction...";
-
+		
 	}
 
 	public void update() {
 		if (Gdx.input.justTouched()) {
-			
+
 			guiCam.unproject(
-					touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0), 
-					game.viewport.x, 
-					game.viewport.y,
-					game.viewport.width, 
-					game.viewport.height
-					);
+					touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0),
+					game.viewport.x, game.viewport.y, game.viewport.width,
+					game.viewport.height);
 
 			if (btnBack.bounds.contains(touchPoint.x, touchPoint.y)) {
 				Assets.playSound(Assets.clickSound);
 				game.setScreen(new MenuScreen(game));
+				return;
+			}
+			if (btnNext.bounds.contains(touchPoint.x, touchPoint.y)) {
+				Assets.playSound(Assets.clickSound);
+				// game.setScreen(new MenuScreen(game));
+				counter = counter + 1;
+				if (counter == 5)
+					counter = 0;
+				textHeadline = Integer.toString(counter) + "/5";
+				textHint = "Hint" + Integer.toHexString(counter);
+				screenshot = "help" + Integer.toHexString(counter) + ".png";
+
 				return;
 			}
 
@@ -60,21 +78,17 @@ public class HelpScreen extends ScreenAdapter {
 	}
 
 	public void draw() {
-		
+
 		GL20 gl = Gdx.gl;
-		
-		gl.glViewport(
-				(int) game.viewport.x, 
-				(int) game.viewport.y, 
-				(int) game.viewport.width,
-				(int) game.viewport.height
-				);
-		
+
+		gl.glViewport((int) game.viewport.x, (int) game.viewport.y,
+				(int) game.viewport.width, (int) game.viewport.height);
+
 		gl.glClearColor(0, 0, 0, 1);
 		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		guiCam.update();
-		
+
 		Assets.font.setScale(0.7f, 0.7f);
 		Assets.font.setColor(new Color(Color.WHITE));
 
@@ -84,17 +98,25 @@ public class HelpScreen extends ScreenAdapter {
 
 		game.batcher.draw(Assets.defaultScreen, 0, 0, Settings.TARGET_WIDTH,
 				Settings.TARGET_HEIGHT);
-		game.batcher.draw(Assets.defaultNotification, 0, 0, Settings.TARGET_WIDTH,
-				Settings.TARGET_HEIGHT);
+		game.batcher.draw(Assets.defaultNotification, 0, 0,
+				Settings.TARGET_WIDTH, Settings.TARGET_HEIGHT);
 
 		game.batcher.draw(Assets.back, btnBack.position.x
 				- ButtonSize.MEDIUM_SQUARE.getButtonWidth() / 2,
 				btnBack.position.y - ButtonSize.MEDIUM_SQUARE.getButtonHeight()
 						/ 2, ButtonSize.MEDIUM_SQUARE.getButtonWidth(),
 				ButtonSize.MEDIUM_SQUARE.getButtonHeight());
+		
+		game.batcher.draw(Assets.back, btnNext.position.x
+				- ButtonSize.MEDIUM_SQUARE.getButtonWidth() / 2,
+				btnNext.position.y - ButtonSize.MEDIUM_SQUARE.getButtonHeight()
+						/ 2, ButtonSize.MEDIUM_SQUARE.getButtonWidth(),
+				ButtonSize.MEDIUM_SQUARE.getButtonHeight());
 
 		Assets.font.setScale(0.6f, 0.6f);
-		Assets.font.drawMultiLine(game.batcher, textString, 60, 350);
+		 Assets.font.drawMultiLine(game.batcher, textHeadline, 60, 350);
+		 Assets.font.drawMultiLine(game.batcher, screenshot, 60, 250);
+		 Assets.font.drawMultiLine(game.batcher, textHint, 60, 150);
 
 		game.batcher.end();
 	}
