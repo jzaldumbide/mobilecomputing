c0eec7968fc5d0dcab60781a5c9e97bac40c712e
jzaldumbide <jzaldumbide@gmail.com>
2014-10-11 11:52:43 +1100

    no changes

--- src/au/edu/unimelb/comp90018/brickbreaker/screens/MultiplayerScreen.java
+++ src/au/edu/unimelb/comp90018/brickbreaker/screens/MultiplayerScreen.java
@@ -7,7 +7,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -18,6 +20,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 import au.edu.unimelb.comp90018.brickbreaker.BrickBreaker;
 import au.edu.unimelb.comp90018.brickbreaker.actors.Button;
@@ -81,7 +84,7 @@ public class MultiplayerScreen extends ScreenAdapter {
 		btnBack = new Button(20, 20, ButtonSize.MEDIUM_SQUARE);
 
 		// network
-		// startBluetooth();
+		 startBluetooth();
 
 		// startServerNetwork();
 		ipAddress = getmyipNetwork();
@@ -304,211 +307,199 @@ public class MultiplayerScreen extends ScreenAdapter {
 	public void startBluetooth() {
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			// startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	//	    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
-
-		Intent discoverableIntent = new Intent(
-				BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-		discoverableIntent.putExtra(
-				BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-		// startActivity(discoverableIntent);
-
-		// IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		// registerReceiver(mReceiver, filter);
-
-		myServer = new ServerThread();
-		myServer.start();
-
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
-				.getBondedDevices();
+		
+		Intent discoverableIntent = new
+				Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+				discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+			//	startActivity(discoverableIntent);
+				
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		//registerReceiver(mReceiver, filter); 
+		
+        myServer = new ServerThread();
+        myServer.start();
+		
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 		if (pairedDevices.size() > 0) {
-			for (BluetoothDevice device : pairedDevices) {
-				String deviceName = device.getName();
-				if (deviceName.equals("Brickbreaker Game")) {
-					isServer = false;
-					// worldView.onScreen = false;
-					ConnectThread myConnection = new ConnectThread(device);
-					myConnection.start();
-					// worldView.connected = true;
-				}
-
-				Log.d("Bluetooth Device:", deviceName);
-			}
+		    for (BluetoothDevice device : pairedDevices) {
+		    	String deviceName = device.getName();
+		    	if(deviceName.equals("Anthony Quattrone (GT-")) {
+		    		isServer = false;
+		    //		worldView.onScreen = false;
+		    		ConnectThread myConnection = new ConnectThread(device);
+		            myConnection.start();
+		      //      worldView.connected = true;
+		    	}
+		    	
+		    	Log.d("Bluetooth Device:", deviceName);
+		    }
 		}
 	}
-
+	
+	
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-				BluetoothDevice device = intent
-						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-				String name = device.getName();
-				Gdx.app.log("Bluetooth Device:", name);
-			}
-		}
-	};
-
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            String name = device.getName();
+	            Log.d("Bluetooth Device:", name);
+	        }
+	    }
+	};	
+	
+	
+	
 	private class ServerThread extends Thread {
-		private final BluetoothServerSocket myServSocket;
+        private final BluetoothServerSocket myServSocket;
 
-		public ServerThread() {
-			BluetoothServerSocket tmp = null;
+        public ServerThread() {
+            BluetoothServerSocket tmp = null;
 
-			try {
-				tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(
-						"myServer", MY_UUID);
-			} catch (IOException e) {
-				Log.e("Bluetooth", "Server establishing failed");
-			}
+            try {
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("myServer", MY_UUID);
+            } catch (IOException e) {
+                Log.e("Bluetooth", "Server establishing failed");
+            }
 
-			myServSocket = tmp;
-		}
+            myServSocket = tmp;
+        }
 
-		public void run() {
-			Log.e("Bluetooth", "Begin waiting for connection");
-			BluetoothSocket connectSocket = null;
-			InputStream inStream = null;
-			OutputStream outStream = null;
-			String line = "";
+        public void run() {
+            Log.e("Bluetooth", "Begin waiting for connection");
+            BluetoothSocket connectSocket = null;
+            InputStream inStream = null;
+            OutputStream outStream = null;
+            String line = "";
 
-			while (true) {
-				try {
-					connectSocket = myServSocket.accept();
-					mBluetoothAdapter.cancelDiscovery();
-				} catch (IOException e) {
-					Log.e("Bluetooth", "Connection failed");
-					break;
-				}
+            while (true) {
+                try {
+                    connectSocket = myServSocket.accept();
+                    mBluetoothAdapter.cancelDiscovery();
+                } catch (IOException e) {
+                    Log.e("Bluetooth", "Connection failed");
+                    break;
+                }
 
-				try {
-					inStream = connectSocket.getInputStream();
-					outStream = connectSocket.getOutputStream();
-					// worldView.outputStream = outStream;
-					// worldView.connected = true;
-					BufferedReader br = new BufferedReader(
-							new InputStreamReader(inStream));
+                try {
+                    inStream = connectSocket.getInputStream();
+                    outStream = connectSocket.getOutputStream();
+                    //inStream.toString();
+                   // worldView.outputStream = outStream;
+                    Gdx.app.log("inStream: ", inStream.toString());
+                    Gdx.app.log("outStream: ", outStream.toString());
+                   // worldView.connected = true;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+                    
+                    while ((line = br.readLine()) != null) {
+                        try {
+                            if(line.startsWith("ShowOnScreen")) {
+                            	//worldView.onScreen = true;
+                            	List<String> coords = Arrays.asList(line.split(","));
+                            	
+                            	float screenWidth = Float.parseFloat(coords.get(1));
+                            	float screenHeight = Float.parseFloat(coords.get(2));
+                            	float x = Float.parseFloat(coords.get(3));
+                            	float y = Float.parseFloat(coords.get(4));
+                            	float xSpeed = Float.parseFloat(coords.get(5));
+                            	float ySpeed = Float.parseFloat(coords.get(6));
+                            	
+                            	//worldView.ball.resetCoords(screenWidth, screenHeight, x, y, xSpeed, ySpeed);
+                            }
+                        	
+                            Log.e("Bluetooth", "Received: " + line);
+                        } catch (Exception e3) {
+                            Log.e("Bluetooth", "Disconnected");
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
 
-					while ((line = br.readLine()) != null) {
-						try {
-							if (line.startsWith("ShowOnScreen")) {
-								/*
-								 * worldView.onScreen = true; List<String>
-								 * coords = Arrays.asList(line .split(","));
-								 * 
-								 * float screenWidth = Float.parseFloat(coords
-								 * .get(1)); float screenHeight =
-								 * Float.parseFloat(coords .get(2)); float x =
-								 * Float.parseFloat(coords.get(3)); float y =
-								 * Float.parseFloat(coords.get(4)); float xSpeed
-								 * = Float.parseFloat(coords.get(5)); float
-								 * ySpeed = Float.parseFloat(coords.get(6));
-								 * 
-								 * worldView.ball.resetCoords(screenWidth,
-								 * screenHeight, x, y, xSpeed, ySpeed);
-								 */
-								Gdx.app.log("Input: ", inStream.toString());
-								Gdx.app.log("Output: ", outStream.toString());
-							}
-
-							Log.e("Bluetooth", "Received: " + line);
-						} catch (Exception e3) {
-							Log.e("Bluetooth", "Disconnected");
-							break;
-						}
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-					break;
-				}
-
-			}
-		}
-	}
-
+            }
+        }
+    }
+	
+	
+	
 	private class ConnectThread extends Thread {
-		private final BluetoothSocket mySocket;
+        private final BluetoothSocket mySocket;
 
-		public ConnectThread(BluetoothDevice device) {
-			BluetoothSocket tmp = null;
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
 
-			try {
-				tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
-			} catch (IOException e) {
-				Log.e("Bluetooth", "Could not connect");
-			}
-			mySocket = tmp;
-		}
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e("Bluetooth", "Could not connect");
+            }
+            mySocket = tmp;
+        }
 
-		public void run() {
-			InputStream inStream = null;
-			OutputStream outStream = null;
-			mBluetoothAdapter.cancelDiscovery();
+        public void run() {
+            InputStream inStream = null;
+            OutputStream outStream = null;
+            mBluetoothAdapter.cancelDiscovery();
 
-			try {
-				mySocket.connect();
-			} catch (IOException e) {
-				Log.e("Bluetooth", this.getName()
-						+ ": Could not establish connection with device");
-				try {
-					mySocket.close();
-				} catch (IOException e1) {
-					Log.e("Bluetooth", this.getName()
-							+ ": could not close socket", e1);
-					this.destroy();
-				}
-			}
+            try {
+                mySocket.connect();
+            } catch (IOException e) {
+                Log.e("Bluetooth", this.getName() + ": Could not establish connection with device");
+                try {
+                    mySocket.close();
+                } catch (IOException e1) {
+                    Log.e("Bluetooth", this.getName() + ": could not close socket", e1);
+                    this.destroy();
+                }
+            }
 
-			String line = "";
-			try {
-				inStream = mySocket.getInputStream();
-				outStream = mySocket.getOutputStream();
+            String line = "";
+            try {
+                inStream = mySocket.getInputStream();
+                outStream = mySocket.getOutputStream();
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+              //  worldView.outputStream = outStream;
+                
+                while ((line = br.readLine()) != null) {
+                    try {
+                        Log.e("Bluetooth", "Received: " + line);
+                        
+                        if(line.startsWith("ShowOnScreen")) {
+                     //   	worldView.onScreen = true;
+                        	List<String> coords = Arrays.asList(line.split(","));
+                        	
+                        	float screenWidth = Float.parseFloat(coords.get(1));
+                        	float screenHeight = Float.parseFloat(coords.get(2));
+                        	float x = Float.parseFloat(coords.get(3));
+                        	float y = Float.parseFloat(coords.get(4));
+                        	float xSpeed = Float.parseFloat(coords.get(5));
+                        	float ySpeed = Float.parseFloat(coords.get(6));
+                        	
+                        	//worldView.ball.resetCoords(screenWidth, screenHeight, x, y, xSpeed, ySpeed);
+                        }
+                    } catch (Exception e3) {
+                        Log.e("Bluetooth", "Disconnected");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						inStream));
-				// worldView.outputStream = outStream;
+        public void cancel() {
+            try {
+                mySocket.close();
+            } catch (IOException e) {
+                Log.e("Bluetooth", this.getName() + ": Could not close socket");
+            }
+        }
+	
+	
 
-				while ((line = br.readLine()) != null) {
-					try {
-						Log.e("Bluetooth", "Received: " + line);
-
-						if (line.startsWith("ShowOnScreen")) {
-							/*
-							 * worldView.onScreen = true; List<String> coords =
-							 * Arrays .asList(line.split(","));
-							 * 
-							 * float screenWidth =
-							 * Float.parseFloat(coords.get(1)); float
-							 * screenHeight = Float .parseFloat(coords.get(2));
-							 * float x = Float.parseFloat(coords.get(3)); float
-							 * y = Float.parseFloat(coords.get(4)); float xSpeed
-							 * = Float.parseFloat(coords.get(5)); float ySpeed =
-							 * Float.parseFloat(coords.get(6));
-							 * 
-							 * worldView.ball.resetCoords(screenWidth,
-							 * screenHeight, x, y, xSpeed, ySpeed);
-							 */
-							Gdx.app.log("Input: ", inStream.toString());
-							Gdx.app.log("Output: ", outStream.toString());
-						}
-					} catch (Exception e3) {
-						Log.e("Bluetooth", "Disconnected");
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		public void cancel() {
-			try {
-				mySocket.close();
-			} catch (IOException e) {
-				Log.e("Bluetooth", this.getName() + ": Could not close socket");
-			}
-		}
-	}
-
-}
+}}
